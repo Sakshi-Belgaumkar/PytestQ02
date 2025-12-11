70e429c313bac26c8eb0fb340d78d20ba30f8f97
Sakshi-Belgaumkar <sakshibelgaumkar04@gmail.com>
2025-12-11 15:44:37 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -6,10 +6,10 @@ def test_format_product():
     result = format_product("P101", "Notebook", 5, 45.50)
 
     expected = (
-        "Product ID   : P101\n"
-        "Name         : Notebook\n"
-        "Quantity     : 5\n"
+        "Product ID   : P101"
+        "Name         : Notebook"
+        "Quantity     : 5"
         "Price        : ₹45.5"
     )
 
-    assert result == expected
+    assert product("P101", "Notebook", 5, 45.5) == expected_output
